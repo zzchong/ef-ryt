ae735631b0ae767a10f4f0a1a5768a1a62982d2d
kaysonyang <kayson_yang@163.com>
2016-01-29 14:41:34 +0800

    app 服务端  增加艺术项目列表查询接口      add by kayson

---
+++ app-wikiServer/src/main/java/com/efeiyi/ec/art/artwork/controller/ArtworkController.java
@@ -0,0 +1,49 @@
+package com.efeiyi.ec.art.artwork.controller;
+
+
+import com.efeiyi.ec.art.model.Artwork;
+import com.ming800.core.base.controller.BaseController;
+import com.ming800.core.does.model.PageInfo;
+import com.ming800.core.does.model.XQuery;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+
+/**
+ * Created by Administrator on 2016/1/29.
+ *
+ */
+@Controller
+public class ArtworkController extends BaseController {
+    private static Logger logger = Logger.getLogger(ArtworkController.class);
+
+
+    @RequestMapping(value = "/app/getArtWorkList.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Map getArtWorkList(HttpServletRequest request) {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+      try{
+          XQuery query = new XQuery("plistArtwork_default", request);
+          PageInfo pageInfo = baseManager.listPageInfo(query);
+          List<Artwork> list = pageInfo.getList();
+          if (list!= null && !list.isEmpty()){
+              resultMap.put("responseInfo",list);
+          }else {
+              resultMap.put("responseInfo",null);
+          }
+          resultMap.put("resultCode","0");
+          resultMap.put("resultMsg","成功");
+        } catch(Exception e){
+            resultMap.put("resultCode", "10004");
+            resultMap.put("resultMsg", "未知错误，请联系管理员");
+            return resultMap;
+        }
+
+        return resultMap;
+    }
+}
