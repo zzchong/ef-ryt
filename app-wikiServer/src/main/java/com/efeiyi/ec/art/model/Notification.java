da41180c8191fd3a5540aa8fd5c54422cbc8f79e
kaysonyang <kayson_yang@163.com>
2016-02-02 13:25:45 +0800

    app 服务端  增加通知    add by kayson

---
+++ app-wikiServer/src/main/java/com/efeiyi/ec/art/model/Notification.java
@@ -0,0 +1,75 @@
+package com.efeiyi.ec.art.model;
+
+import com.efeiyi.ec.art.organization.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/2/2.
+ *
+ */
+@Entity
+@Table(name = "app_notification")
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
+public class Notification implements Serializable {//系统通知
+    private String id;
+    private String content;
+    private User targetUser;
+    private Date createDatetime;
+    private Artwork artwork;
+
+
+
+    @Id
+    @GenericGenerator(name = "id", strategy = "com.ming800.core.p.model.M8idGenerator")
+    @GeneratedValue(generator = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "content")
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User getTargetUser() {
+        return targetUser;
+    }
+
+    public void setTargetUser(User targetUser) {
+        this.targetUser = targetUser;
+    }
+    @Column(name = "create_time")
+    public Date getCreateDatetime() {
+        return createDatetime;
+    }
+
+    public void setCreateDatetime(Date createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "art_work_id")
+    public Artwork getArtwork() {
+        return artwork;
+    }
+
+    public void setArtwork(Artwork artwork) {
+        this.artwork = artwork;
+    }
+}
+
+
