53100602af12a4adb5c48e39dc7f7840a2b31d65
zzchong <zz_chong@163.com>
2016-05-26 11:00:21 +0800

    分享页面

--- app-wikiServer/src/main/java/com/efeiyi/ec/art/h5/controller/HArtWorkController.java
+++ app-wikiServer/src/main/java/com/efeiyi/ec/art/h5/controller/HArtWorkController.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
@@ -46,7 +47,7 @@ public class HArtWorkController extends BaseController {
     private final static String VIEW = "/shareView";
 
     @RequestMapping("/app/toShareView.do")
-    @JsonIgnore
+    @ResponseBody
     public Map createView(HttpServletRequest request) throws Exception{
         LogBean logBean = new LogBean();
         logBean.setApiName("toShareView");
