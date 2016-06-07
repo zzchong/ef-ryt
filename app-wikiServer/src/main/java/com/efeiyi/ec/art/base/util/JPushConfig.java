c44dd99409449c7d070671fbf28097157d707fe1
zzchong <zz_chong@163.com>
2016-06-07 18:52:47 +0800

    pa

--- app-wikiServer/src/main/java/com/efeiyi/ec/art/base/util/JPushConfig.java
+++ app-wikiServer/src/main/java/com/efeiyi/ec/art/base/util/JPushConfig.java
@@ -4,6 +4,6 @@ package com.efeiyi.ec.art.base.util;
  * Created by Administrator on 2016/4/15.
  */
 public class JPushConfig {
-    public static final String appKey ="539b73fd73c82f1134120a57";
-    public static final String masterSecret = "fea912e000db6a462048f9ef";
+    public static final String appKey ="cd59fb03104872416946dd1d";
+    public static final String masterSecret = "bcae78b377dd55f3fd2fb3fc";
 }
